b418fb19ca69246ef1f7c09ce108a6ca30ed11d9
Mathieu Griffoul <mathieu.griffoul@zenika.com>
2021-02-05 15:04:02 +0100

    Organize folders

---
+++ src/components/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="src/commons/img/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Home extends Vue {}
+</script>
